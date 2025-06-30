30b4a58793f780dc1048f8c9bfd46b65fd1af30e
masonturner <msntrnrdev@gmail.com>
2025-06-30 13:32:59 -0400

    Adds footer copyright

--- src/common/components/layout/Footer.tsx
+++ src/common/components/layout/Footer.tsx
@@ -1,3 +1,8 @@
 export const Footer = () => {
-  return <footer className="max-w-screen-xl mx-auto w-full py-4 px-6">TODO: footer</footer>;
+  const year = new Date().getFullYear();
+  return (
+    <footer className="max-w-screen-xl mx-auto w-full py-4 px-6">
+      <span className="font-bold text-sm">©{year} Mason Turner</span>
+    </footer>
+  );
 };
