e928196d5796e869a559c8383fbff0d6cc19e6ad
masonturner <msntrnrdev@gmail.com>
2025-06-30 15:58:45 -0400

    Adds a GitHub link to the footer

--- src/common/components/layout/Footer.tsx
+++ src/common/components/layout/Footer.tsx
@@ -1,8 +1,14 @@
 export const Footer = () => {
   const year = new Date().getFullYear();
   return (
-    <footer className="max-w-screen-xl mx-auto w-full py-4 px-6">
+    <footer className="flex justify-between items-center max-w-screen-xl mx-auto w-full py-4 px-6">
       <span className="font-bold text-sm">©{year} Mason Turner</span>
+      <div>
+        <a href="https://www.github.com/tb9-mason" target="_blank" rel="noreferrer">
+          <img src="github-mark-white.svg" alt="GitHub" className="w-5" />
+          <span className="sr-only">View on GitHub</span>
+        </a>
+      </div>
     </footer>
   );
 };
